76a4344be6284150fed39484b2a41dd533bd436d
Johnny Cai <jaihysc@gmail.com>
2020-09-21 16:42:51 -0400

    Setup project

---
+++ app/src/test/java/com/hyperion/ExampleUnitTest.java
@@ -0,0 +1,21 @@
+/*
+ * This file is subject to the terms and conditions defined in 'LICENSE' in the source code package
+ */
+
+package com.hyperion;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
