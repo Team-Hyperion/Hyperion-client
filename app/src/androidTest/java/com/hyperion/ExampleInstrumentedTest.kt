aa7a386a1c8b60533b9c4a9aaa88858ea6b1475a
Andy Chen <chen7944@gmail.com>
2020-11-05 15:39:35 -0500

    migrate code to this repo

---
+++ app/src/androidTest/java/com/hyperion/ExampleInstrumentedTest.kt
@@ -0,0 +1,29 @@
+/*
+ * This file is subject to the terms and conditions defined in 'LICENSE' in the source code package
+ */
+
+package com.hyperion;
+
+import android.content.Context;
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.hyperion", appContext.packageName)
+    }
+}
